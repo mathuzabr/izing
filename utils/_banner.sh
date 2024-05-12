0b3bc2b55fb63c151820f950745a70b48cd601fb
Pack Typebot <mathuza@gmail.com>
2024-05-11 21:51:04 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -21,12 +21,17 @@ print_banner() {
 
   printf "\n\n"
 
-  printf "${GREEN}";
-  printf "\n";
-  printf "\n";
-  printf "\n";
-  printf "\n";
-  printf "${NC}";
+printf "${GREEN}";  
+printf "  _____        _____ _  __  _________     _______  ______ ____   ____ _______ \n";
+printf " |  __ \ /\   / ____| |/ / |__   __\ \   / /  __ \|  ____|  _ \ / __ \__   __|\n";
+printf " | |__) /  \ | |    | ' /     | |   \ \_/ /| |__) | |__  | |_) | |  | | | |   \n";
+printf " |  ___/ /\ \| |    |  <      | |    \   / |  ___/|  __| |  _ <| |  | | | |   \n";
+printf " | |  / ____ \ |____| . \     | |     | |  | |    | |____| |_) | |__| | | |   \n";
+printf " |_| /_/    \_\_____|_|\_\    |_|     |_|  |_|    |______|____/ \____/  |_|   \n";
+printf "                                                                              \n";  
+printf "\n";
+printf "packtypebot.com.br\n";
+printf "${NC}";
 
-  printf "\n"
+printf "\n"
 }
